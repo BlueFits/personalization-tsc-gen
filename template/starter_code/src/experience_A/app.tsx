ab4854ef1ca850f394af2c27271e0f0fb94c3592
bluefits <christianftw@ymail>
2022-10-17 11:02:12 -0400

    Moved shared resource to common

--- template/starter_code/src/experience_A/app.tsx
+++ template/starter_code/src/experience_A/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';//Needed wherever react is used
 import constants from "./constants/default.constants";
-import { tryCatch } from "./utils/default.utils";
-import { createReactRoot } from "./utils/react.utils";
+import { tryCatch } from "../common/utils/default.utils";
+import { createReactRoot } from "../common/utils/react.utils";
 
 //Components
 import Sample from './components/Sample/Sample';
