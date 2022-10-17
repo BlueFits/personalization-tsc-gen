73ceee92b7ac2bd4b3a284543029964c5a3da52d
bluefits <christianftw@ymail>
2022-10-16 23:02:58 -0400

    added boiler plate component

--- src/experience_A/components/Sample/Sample.tsx
+++ src/experience_A/components/Sample/Sample.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-const Sample = () => (
-    <div
-      className="sample_style"
-    >
-      <h1>
-        Hello World from <a href="https://www.npmjs.com/package/@bluefits/pers-tsc-gen">PERS-TSC-GEN</a>
-      </h1>
-    </div>
-);
+const Sample = () => {
+	const [clicked, isClicked] = useState(false);
+	return (
+		<div
+		className="sample_style"
+		>
+			<h1>
+				Hello PERS <span className="span_text" onClick={() => isClicked(!clicked)}>{ clicked ? ":)" : "CLICK ME" }</span>
+			</h1>
+		</div>
+	);
+};
 
 export default Sample;
